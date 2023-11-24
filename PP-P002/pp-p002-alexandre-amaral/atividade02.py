64acffe8d07d7faa2d6835510943943c9d1a328f
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-11-24 15:54:12 -0300

    criado crud na atividade 2

---
+++ PP-P002/pp-p002-alexandre-amaral/atividade02.py
@@ -0,0 +1,29 @@
+
+def main():
+    id
+    while True:
+        print("////////// MENU //////////")
+        print("1- Adcionar Tarefa")
+        print("2- Marcar como Realizada")
+        print("3- Editar Tarefa")
+        print("4- Listar As Tarefas")
+        print("0- Sair do programa")
+        op = int(input())
+
+        match op:
+            case 0:
+                
+                break
+            case 1:
+                break
+            case 2:
+                break
+            case 3:
+                break
+            case 4:
+               break
+            case _:
+                print("Opção inválida")
+                
+if __name__ == "__main__":
+    main()
