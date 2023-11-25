68e291c1c044ce9a59a810c11ec6fb301958c8d3
leogmc <ifbaianoleo@gmail.com>
2023-11-25 19:15:39 -0300

    Reestruturar a pasta e enviar PP-P001 com as funcoes inserirProduto e excluirProduto

---
+++ Semana3/Leandro/PP-P003/main.py
@@ -0,0 +1,50 @@
+from Supermercado import f_inserir_produto
+from Supermercado import f_excluir_produto
+
+def main():
+    while True:
+
+        print("========== Supermercado Bem Melhor ==========")
+        print(" ")
+        print("[1] - Inserir novo produto: ")
+        print("[2] - Excluir um produto: ")
+        print("[3] - Listar produtos: ")
+        print("[4] - Consultar preço (através do código do produto): ")
+        print("[0] - Sair. ")
+
+        opcao = int(input("Escolha uma opção: "))
+        print(" ")
+
+        if opcao == 1:
+            f_inserir_produto.inserirProduto()
+            print(" ")
+                    
+        elif opcao == 2:
+            f_excluir_produto.excluirProduto()
+            print(" ")  
+                     
+        elif opcao == 3:
+            print("3")
+            print(" ")
+            
+        elif opcao == 4:
+            print("4")
+            print(" ")
+            
+        elif opcao == 0:
+            print("Programa finalizado. Até a próxima!")
+            exit(0)
+        else:
+            print("Opcão inválida. Tente novamente.")
+            print(" ")
+
+
+  
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
