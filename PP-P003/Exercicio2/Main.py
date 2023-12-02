44d90e2bc1f1e9667ea8945b849fb492c119a98a
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-12-02 20:18:20 -0300

    criado crud no main da atividade2

---
+++ PP-P003/Exercicio2/Main.py
@@ -0,0 +1,28 @@
+def main():
+        
+    op = -1
+    while(op != 0):
+        print("/////////////* MENU */////////////")
+        print("1- Adcionar Funcionarios")
+        print("2- Listar Funcionarios")
+        print("3- Excluir Funcionarios")
+        print("4- Consultar Funcionarios")
+        print("5- Reajuste")
+        print("6- Sair")
+        op = int(input())
+        match op:
+            case 1:
+                break
+            case 2:
+                break
+            case 3:
+                break
+            case 4:
+                break
+            case 5:
+                break
+            case 6:
+                break
+    
+if __name__ == "__main__":
+    main()
