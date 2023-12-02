9a2617e9af44f7095dd86f678857fe536bb49089
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-12-02 19:50:19 -0300

    criado estrutura de diretorios e crud da atividade supermercado

---
+++ PP-P003/supermercado/main.py
@@ -0,0 +1,23 @@
+def main():
+    produtos = []
+    op = -1
+    while(op != 0):
+        print("/////////////* MENU */////////////")
+        print("1- Inserir Novo Produto")
+        print("2- Excluir Produto")
+        print("3- Listar Produtos")
+        print("4- Consultar Produto")
+        print("0- Sair")
+        op = int(input())
+        match op:
+            case 1:
+                break
+            case 2:
+                break
+            case 3:
+                break
+            case 4:
+                break
+    
+if __name__ == "__main__":
+    main()
